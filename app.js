bedfb070d1c427eca99347e9dc8b82b4fdc960f7
dmutaasa1 <dexdj256@gmail.com>
2026-02-21 19:32:43 +0300

    Configure Railway deployment

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use(session({
 
 // MySQL connection pool
 const dbConfig = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '0785414615',
-  database: process.env.DB_NAME || 'st_kizito',
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME ,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
